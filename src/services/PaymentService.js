2aa4ee1a0919525f18e2e2df094e8c6f029a4e14
devFresher <fresher.dev01@gmail.com>
2023-03-05 09:41:53 +0100

    paystack integration

--- src/services/PaymentService.js
+++ src/services/PaymentService.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
+import { config } from 'winston';
 
 export default class PaymentService {
-    static async processCardPayment(cardNumber, expirationMonth, expirationYear, cvv, amount) {
+
+    static async #createChargeToken(cardNumber, expirationMonth, expirationYear, cvv) {
         const response = await axios.post(
-            'https://api.paystack.co/charge',
+            'https://api.paystack.co/charge/tokenize',
             {
                 card: {
                     number: cardNumber,
@@ -11,6 +13,25 @@ export default class PaymentService {
                     expiry_month: expirationMonth,
                     expiry_year: expirationYear,
                 },
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${config.get(PAYSTACK_SECRET_KEY)}`,
+                },
+            },
+        );
+        if (response.data.status === 'success') {
+            return response.data.data.authorization_code;
+        } else {
+            throw new Error(`Creating charge token failed: ${response.data.message}`);
+        }
+    }
+
+    static async chargeToken(authorizationCode, amount) {
+        const response = await axios.post(
+            'https://api.paystack.co/charge',
+            {
+                authorization_code: authorizationCode,
                 amount: amount * 100, // Paystack amount is in kobo (1 NGN = 100 kobo)
             },
             {
@@ -24,5 +45,9 @@ export default class PaymentService {
         } else {
             throw new Error(`Payment failed: ${response.data.message}`);
         }
+    }
+    static async processCardPayment(cardNumber, expirationMonth, expirationYear, cvv, amount) {
+        const authCode = this.#createChargeToken(cardNumber, expirationMonth, expirationYear, cvv)
+        charge
     }
 }
